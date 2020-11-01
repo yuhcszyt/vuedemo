ae613c7c113f1c646376bd9734aec73cd4637229
yuhcszyt <2502203@163.com>
2020-11-01 23:07:43 +0800

    vue

---
+++ learnVue01/learnV0101/12-前端模块化/01-ES6的模块化实现/aaa.js
@@ -0,0 +1,8 @@
+var name='xiaoming';
+var flag=true;
+function sum(num1,num2) {
+    return num1+num2;
+}
+export {
+    flag,sum
+}
---
+++ learnVue01/learnV0101/12-前端模块化/01-ES6的模块化实现/mmm.js
@@ -0,0 +1,10 @@
+import {flag,sum} from "./aaa.js";
+import {Person} from "./bbb.js";
+import defaultdata from "./bbb.js";
+
+if(flag){
+    console.log(sum(32, 24));
+}
+let p=new Person();
+p.run();
+defaultdata();
