8ba687e0c4bb41b9425c8b369c4abe69507a99d3
yuhcszyt <2502203@163.com>
2020-11-08 01:11:57 +0800

    vue
    Signed-off-by: yuhcszyt <2502203@163.com>

---
+++ learnVue01/learnV0102/01-webpack的使用/05-webpack的插件配置/src/vue/cpn.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>i am cpm</h2>
+        <h2>{{name}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "cpn",
+        data(){
+            return {
+                name: "yuhc"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
