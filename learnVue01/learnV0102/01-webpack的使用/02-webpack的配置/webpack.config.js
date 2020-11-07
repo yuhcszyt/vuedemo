ed8051f74e0f36c0deb9f4021a8bef703393a840
yuhcszyt <2502203@163.com>
2020-11-07 11:22:56 +0800

    vue

--- learnVue01/learnV0102/01-webpack的使用/02-webpack的配置/webpack.config.js
+++ learnVue01/learnV0102/01-webpack的使用/02-webpack的配置/webpack.config.js
@@ -5,6 +5,13 @@ module.exports = {
     output: {
         path:path.resolve(__dirname,'dist'),
         filename:'bundle.js'
-    }
-
+    },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [ 'style-loader', 'css-loader' ]
+                }
+            ]
+        }
 }
