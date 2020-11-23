88e4a5b98a0abddaf660d0dc438ad1d18ee5547d
yuhcszyt <2502203@163.com>
2020-11-24 00:12:10 +0800

    创建
    Signed-off-by: yuhcszyt <2502203@163.com>

---
+++ learnVue/learnV0102/01-webpack的使用/03-webpack的loader/src/main.js
@@ -0,0 +1,10 @@
+const {sum,mul} = require('./js/mathUtils');
+
+console.log(sum(23, 43));
+console.log(mul(2, 4));
+
+require('./css/normal.css');
+
+require('./css/special.less');
+
+document.writeln("liyinhe");
