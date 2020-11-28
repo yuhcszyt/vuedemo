34d5f7b7546af8a9d5807c54b3cbf357bcfcdd4f
yuhcszyt <2502203@163.com>
2020-11-28 14:55:14 +0800

    创建
    Signed-off-by: yuhcszyt <2502203@163.com>

---
+++ learnVue/learnVuejs06/learnvuerouter/src/router/index.js
@@ -0,0 +1,16 @@
+import VueRouter from "vue-router";
+import Vue from 'vue'
+
+//安装插件
+Vue.use(VueRouter);
+
+const routes=[
+
+
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export  default routes
